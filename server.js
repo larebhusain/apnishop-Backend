0c5165df55f998ae9fa88856dc7c6baaaa79e0d3
Laraib Hussain <laraibhussain078600@gmail.com>
2026-04-17 01:17:47 +0530

    change second

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import cors from "cors";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import contactRoutes from "./routes/contactRoutes.js";
+import path from 'path'
 //configure env
 dotenv.config();
 
@@ -18,9 +19,15 @@ connectDB();
 const app = express();
 
 //middelwares
-app.use(cors());
+app.use(cors(
+  {
+  origin: 'https://apnishop-frontend.vercel.app',
+  credentials: true,
+  }
+));
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, './client/build')))
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -28,10 +35,12 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 app.use("/api/v1/contact", contactRoutes);
 //rest api
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to ecommerce app</h1>");
+// app.get("/", (req, res) => {
+//   res.send("<h1>Welcome to ecommerce app</h1>");
+// });
+app.use('*', function(req, res){
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
-
 //PORT
 const PORT = process.env.PORT || 8080;
 
